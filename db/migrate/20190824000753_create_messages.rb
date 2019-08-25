46d41776737b0fa220f11be8cda25af327c8d07f
Leonardo Abreu <leonardo.abreu@inf.ufrgs.br>
2019-08-25 02:55:21 -0300

    feat: news scripts to crate.js
    - Configurated create.js to clear textearea and background color
    - Configurated migration to message not null

--- db/migrate/20190824000753_create_messages.rb
+++ db/migrate/20190824000753_create_messages.rb
@@ -1,7 +1,7 @@
 class CreateMessages < ActiveRecord::Migration[5.2]
   def change
     create_table :messages do |t|
-      t.text :message
+      t.text :message, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2019_08_24_000753) do
 
   create_table "messages", force: :cascade do |t|
-    t.text "message"
+    t.text "message", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
